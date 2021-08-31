ec01176600cf3e47e0a35a216149cda5de682d4d
Nex <89796047+Anexity@users.noreply.github.com>
2021-08-31 16:03:53 +0200

    change window format & clean up

--- multitool.py
+++ multitool.py
@@ -15,7 +15,7 @@ input1 = tk.Label(
     fg="white",
     bg="#171717",
     width=26,
-    height=2
+    height=3
 )
 
 e1 = tk.Entry(
@@ -29,7 +29,7 @@ input2 = tk.Label(
     fg="white",
     bg="#171717",
     width=26,
-    height=2
+    height=3
 )
 
 e2 = tk.Entry(
@@ -43,7 +43,7 @@ greeting = tk.Label(
     fg="white",
     bg="#171717",
     width=26,
-    height=2
+    height=3
 )
 
 # ipcheck input side - lit
@@ -52,7 +52,7 @@ iptex1 = tk.Label(
     fg="white",
     bg="#171717",
     width=26,
-    height=2
+    height=3
 )
 
 ipe1 = tk.Entry(
@@ -66,7 +66,7 @@ iptex2 = tk.Label(
     fg="white",
     bg="#171717",
     width=26,
-    height=2
+    height=3
 )
 
 ipe2 = tk.Entry(
@@ -80,7 +80,7 @@ iptex3 = tk.Label(
     fg="white",
     bg="#171717",
     width=26,
-    height=2
+    height=3
 )
 
 ipe3 = tk.Entry(
@@ -96,8 +96,8 @@ button1 = tk.Button(
     text="Password Generator",
     width=25,
     height=3,
-    bg="#999999",
-    fg="#171717",
+    bg="#191919",
+    fg="#FFFFFF",
 )
 
 def handle_click(event):
@@ -128,14 +128,14 @@ button2 = tk.Button(
     text="diceroll",
     width=25,
     height=3,
-    bg="#999999",
-    fg="#171717",
+    bg="#191919",
+    fg="#FFFFFF",
 )
 
 
 # Dicethrow button and build with
 # random randint.
-
+)
 def handle_click2(event):
     throw1 = random.randint(1, int(e2.get()))
 
@@ -150,12 +150,14 @@ button3 = tk.Button(
     text="check ip",
     width=25,
     height=3,
-    bg="#999999",
-    fg="#171717",
+    bg="#191919",
+    fg="#FFFFFF",
 )
 
 def handle_click3(event):
     webvar = (ipe1.get())
+    portnum = int(ipe2.get())
+
     if(webvar != ""):
         for ping in range(int(ipe2.get()), int(ipe3.get())):
             adress = (webvar) + ":" + str(ping)
@@ -163,37 +165,42 @@ def handle_click3(event):
             if res == 0:
                 tkinter.messagebox.showinfo(
                     title=("ping process"),
-                    message=("a ping went through to host.")
+                    message=("a ping went through to host on port " + str(portnum))
                 )
+                portnum = portnum + 1
             elif res == 2:
                 tkinter.messagebox.showerror(
                     title=("ping process"),
-                    message=("no response was recieved.")
-                    )
+                    message=("no response was recieved on port " + str(portnum))
+                )
+                portnum = portnum + 1
             else:
                 tkinter.messagebox.showerror(
                     title=("ping process"),
-                    message=("pinging the host failed.")
+                    message=("pinging the host failed on port " + str(portnum))
                 )
+                portnum = portnum + 1
 
 # packing all the windows
 
-greeting.pack()
-input1.pack()
-e1.pack()
-input2.pack()
-e2.pack()
+window.title("Nex' Multitool")
+greeting.grid(row=0, column=2)
+input1.grid(row=0)
+e1.grid(row=0, column=1)
+input2.grid(row=1, column=0)
+e2.grid(row=1, column=1)
 button1.bind("<Button-1>", handle_click)
 button2.bind("<Button-1>", handle_click2)
-button1.pack()
-button2.pack()
-iptex1.pack()
-ipe1.pack()
-iptex2.pack()
-ipe2.pack()
-iptex3.pack()
-ipe3.pack()
+button1.grid(row=0, column=2)
+button2.grid(row=1, column=2)
+iptex1.grid(row=2, column=0)
+ipe1.grid(row=2, column=1)
+iptex2.grid(row=3, column=0)
+ipe2.grid(row=3, column=1)
+iptex3.grid(row=4, column=0)
+ipe3.grid(row=4, column=1)
 button3.bind("<Button-1>", handle_click3)
-button3.pack()
+button3.grid(row=4, column=2)
+window.configure(bg="#171717")
 
 window.mainloop()
